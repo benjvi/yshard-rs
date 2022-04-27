819f4a81f4f5a851ea2c6717bfeb358ef714b1a6
Benjamin Vickers <bvickers@vmware.com>
2022-04-27 10:33:09 +0200

    Include libjq in binary using jq-sys `bundled` feature
    Closes #11
    Needed to pin `clap` dep to build - opened issue #12

--- hack/build-linux-amd64.sh
+++ hack/build-linux-amd64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # required deps: 
-# sudo apt install libjq1 libjq-dev libonig-dev gcc rustc
+# sudo apt install libjq1 libjq-dev libonig-dev gcc rustc libtool
 
 export JQ_LIB_DIR=/usr/lib/x86_64-linux-gnu/
 cargo build --release
