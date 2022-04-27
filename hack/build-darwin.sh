f9397ea861b40f110dd9f99ec64e5b2779d7724e
Benjamin Vickers <bvickers@vmware.com>
2022-04-27 13:31:43 +0200

    document deps for linux arm / mac builds

--- hack/build-darwin.sh
+++ hack/build-darwin.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# xcode C environment has been seen to have incompatibilities with jq code, need to take care with that combination
+# required deps:
+# brew install autoconf automake jq
+
 export JQ_LIB_DIR=/usr/local/Cellar/jq/1.6/lib
 cargo build --release
--- hack/build-linux-arm64.sh
+++ hack/build-linux-arm64.sh
@@ -1,2 +1,6 @@
+
+# required deps:
+# sudo apt install libjq1 libjq-dev libonig-dev gcc rustc libtool
+
 export JQ_LIB_DIR=/usr/lib/aarch64-linux-gnu/
 cargo build --release
